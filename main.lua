64d0e85efafe1bd5a8ac9c5e13dcf7331dab7b8c
ArtZabAZ <migor1103@gmail.com>
2025-01-05 14:48:58 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 286090429 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/aartzz/jailcore/main/arsenal.lua", true))()
+else
+  game.Players.LocalPlayer:Kick("Game not supported.")
