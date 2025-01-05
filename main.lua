97438c421bb7c2f5e135968d2abd5714e4377af8
ArtZabAZ <migor1103@gmail.com>
2025-01-05 14:55:53 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,1 @@
-if game.PlaceId == 286090429 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/aartzz/jailcore/main/arsenal.lua", true))()
-else
-  game.Players.LocalPlayer:Kick("Game not supported.")
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aartzz/jailcore/main/arsenal.lua", true))()
